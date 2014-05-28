1554028fe0a8b0c99cdc479defba51f89933e4fd
Kamil Smuga <smugakamil@gmail.com>
2014-05-28 22:50:19 +0100

    docker-io dependencies sorted in puppet manifest

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -1,3 +1,17 @@
 class { 'yum':
 }
-package { 'docker-io': ensure => present }
+
+package { 'docker-io': 
+        require => Yumrepo[ "epel" ],
+        ensure => "installed",
+}
+
+service { 'docker':
+        ensure => "running",
+        enable => true,
+        require => Package['docker-io'],
+}
+
+service { 'puppet':
+        enable => true,
+        }
